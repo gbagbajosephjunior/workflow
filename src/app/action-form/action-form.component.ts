3e646ca3e944073c36e4623e57b07dc48f92e7ca
Gbagba Joseph Junior <gbagbajosephjunior@gmail.com>
2024-02-12 09:06:34 +0100

    hd

--- src/app/action-form/action-form.component.ts
+++ src/app/action-form/action-form.component.ts
@@ -6,19 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./action-form.component.css']
 })
 export class ActionFormComponent implements OnInit {
-  // showComponent = false;
+  isContainerHidden = false;
+  showForm = false;
+  StatClick = false
+  FormClick = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  // toggleComponent(): void {
-  //   this.showComponent = true;
-  // }
+  
+ 
 
-  isContainerHidden: boolean = false;
-
-  hideContainer(): void {
+  hideContainer() {
     this.isContainerHidden = true;
+  }
+
+  showFormOnClick() {
+    this.isContainerHidden = true; // Hide container
+    this.showForm = true; // Show form
+    this.FormClick = true;
+  }
+
+ 
+  showFormClick(){
+    this.FormClick = true;
+  }
+  onStatistics() {
+    this.StatClick = true; // Set StatClick to true when statistics button is clicked
   }
 }
